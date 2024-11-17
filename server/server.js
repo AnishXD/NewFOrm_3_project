e277bb2394ff0a10ecfb82364ee39a91cd13a83a
AnishXD <110338891+AnishXD@users.noreply.github.com>
2024-11-17 15:41:37 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 const corsOptions = {
-    origin: 'http://localhost:5173', // Allow requests from your frontend's origin
+    origin: 'https://new-f-orm-3-project.vercel.app', // Allow requests from your frontend's origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
   };
